6c85d064e63bfb090f27b39940258f0fbbfab2bd
YanniWangAnnie <yanniwangannie@gmail.com>
2018-03-11 19:44:42 -0700

    upload file, write info to table

--- app.py
+++ app.py
@@ -1,30 +1,65 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, flash
+from werkzeug.utils import secure_filename
+import os
+import time
+from azure.storage.blob import BlockBlobService, ContentSettings
 from azure.cosmosdb.table.tableservice import TableService
 from azure.cosmosdb.table.models import Entity
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+block_blob_service = BlockBlobService(account_name='photofunstorage', account_key='Br6qGU0woc+qOQtsneQ6XkgQx6gsmcvmbg9Eyh6+gpISHwmu48o+rmBzIQvOkYfho5FM3xsDP1TrKWVr08XQMg==')
+table_service = TableService(account_name='photofunstorage', account_key='Br6qGU0woc+qOQtsneQ6XkgQx6gsmcvmbg9Eyh6+gpISHwmu48o+rmBzIQvOkYfho5FM3xsDP1TrKWVr08XQMg==')
+
+
+UPLOAD_FOLDER = 'temp'
+ALLOWED_EXTENSIONS = set(['jpg','jpeg'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'Thisisasecret'
+
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def index():
-
-    #art=Art()
-    #art.style=request.form.get()
-    #art.original=request.form.get()
-    #art.contact=request.form.get("email")
     return render_template("index.html")
 
-@app.route("/steps", methods=['post'])
-def add_art():
-    art =  Entity()
-    art.PartitionKey 
-    art.RowKey 
-    art.style = request.form.get()
-    art.origin = request.form.get()
-    art.contact = request.form.get('email')
-    art.status = New
-    table_service.insert_entity('photoart',art)
-    return render_template("congrt.html")
+@app.route("/nextstep", methods=['GET','POST'])
+def nextstep():
+    styleid = request.args.get('styleid')
+  
+
+    if request.method == "POST":
+        f = request.files['file']
+        if f.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            blobname = str(time.time())
+            block_blob_service.create_blob_from_path(
+                'photos', blobname,'temp/'+filename, content_settings=ContentSettings(content_type='image/jpg')
+            )
+        art =  Entity()
+        art.PartitionKey = 'seattle'
+        art.RowKey = str(time.time())
+        art.styleid = styleid
+        art.blobid = blobname
+        art.contact = request.form['emailaddr']
+        table_service.insert_entity('photoart',art)  
+        return "Congratulations!"
+    return render_template("nextstep.html", styleid=styleid)
+
 
 @app.route("/completion")
 def congrt():
     return "congratulation"
+
+if __name__ == '__main__':
+    app.run(debug=True)
