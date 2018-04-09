492cd77ebc10cd3b80a48ba05dc0334910404747
YanniWangAnnie <yanniwangannie@gmail.com>
2018-04-08 21:35:30 -0700

    use virtualenv

--- web/app.py
+++ web/app.py
@@ -5,9 +5,6 @@ import time
 from azure.storage.blob import BlockBlobService, ContentSettings
 from azure.cosmosdb.table.tableservice import TableService
 from azure.cosmosdb.table.models import Entity
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
 
 block_blob_service = BlockBlobService(account_name='photofunstorage', account_key='Br6qGU0woc+qOQtsneQ6XkgQx6gsmcvmbg9Eyh6+gpISHwmu48o+rmBzIQvOkYfho5FM3xsDP1TrKWVr08XQMg==')
 table_service = TableService(account_name='photofunstorage', account_key='Br6qGU0woc+qOQtsneQ6XkgQx6gsmcvmbg9Eyh6+gpISHwmu48o+rmBzIQvOkYfho5FM3xsDP1TrKWVr08XQMg==')
